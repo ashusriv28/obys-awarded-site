56b74d89bcdf80bb2381b6a0f7202ebe8b4bafd4
amitsriv2 <amitsriv2@gmail.com>
2024-03-16 21:49:06 +0530

    page1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimate() {
+    var tl = gsap.timeline();
 
 tl.from(".line h1", {
     y: 150,
@@ -24,19 +25,41 @@ tl.from("#line1-part1", {
 tl.to('.line h2',{
     animationName:"anime",
     opacity:1
-})
+});
 tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 4
+    delay: 0
 });
 tl.from("#page1",{
     delay:0.2,
     y:1600,
     opacity:0,
-    duration:0.5,
+    duration:0.6,
     ease:power4
-})
+});
 tl.to("loader",{
     display: "none"
+});
+tl.from("#nav",{
+   opacity:0
 })
+  
+tl.from("#hero h1,#hero h2,#hero h3,#hero h4",{
+    y:120,
+    stagger:0.2
+})
+}
+
+function cursorAnimation() {
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#cursor",{
+          left:dets.x,
+          top:dets.y
+        })
+      })
+    
+      Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimate();
+cursorAnimation()
